2b78a3969ee0d06503af2c619135a3992bf4af83
Aanod <Aanod.mohamed@georgebrown.ca>
2026-01-18 23:28:58 -0500

    add lab 1 complete

---
+++ Week01/lab1.py
@@ -0,0 +1,30 @@
+# Sample coding question 01 Week 01
+# Aanod Mohamed
+
+# Question 2: defining an array (list)
+my_array = [1,4,7,9]
+
+
+
+# Question 3:
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+print(e)
+
+
+# Question 4: 
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:",userAge)
